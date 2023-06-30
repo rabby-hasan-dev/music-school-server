16b7e1a14be1f151031b0213eeeeba91f939a33d
rabby6530 <you@example.com>
2023-06-30 16:58:18 +0600

    added instructor api

--- index.js
+++ index.js
@@ -121,6 +121,33 @@ async function run() {
             const result = await allUsersCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
+        //instructor related api
+        app.get('/allUsers/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false })
+            }
+
+            const query = { email: email };
+            const user = await allUsersCollection.findOne(query);
+            const result = { instructor: user?.role === 'instructor' };
+            res.send(result);
+        })
+
+
+        app.patch('/allUsers/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await allUsersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
 
 
 
